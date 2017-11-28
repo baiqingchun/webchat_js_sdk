66380621bfc4c794729e22e06509800dcc1ea122
dev <qwer123456>
2017-11-28 13:51:44 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    // 输入你的配置
+    return {
+     /*   appId: 'wx8ddedaeb1b6a9546',
+        appSecret: '26f157015d7b3e20b40b94cf41a038ff', */
+        appId: 'wx0b2a81ef8f12efdf',
+        appSecret: 'fa919b0abbc749de7917862d51c5670b',
+        appToken: 'token'
+    };
+};
